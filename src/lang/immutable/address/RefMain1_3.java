a1c925abf4b67fdfc411e62fdf125e4ba13f2832
Lee-HyunSoo <cris_lx@naver.com>
2024-07-08 22:18:07 +0900

    immutable

---
+++ src/lang/immutable/address/RefMain1_3.java
@@ -0,0 +1,21 @@
+package lang.immutable.address;
+
+public class RefMain1_3 {
+
+    public static void main(String[] args) {
+        Address a = new Address("서울");
+        Address b = a;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        //change 메서드만 보고는 사이드이펙트 예상 불가
+        change(b, "부산");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+
+    private static void change(Address address, String changeAddress) {
+        System.out.println("주소 값을 변경합니다 -> " + changeAddress);
+        address.setValue(changeAddress);
+    }
+}
