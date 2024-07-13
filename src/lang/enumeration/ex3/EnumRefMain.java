e30c3d3d38ddd574d1009316c72c25a5b67fbe72
Lee-HyunSoo <cris_lx@naver.com>
2024-07-13 18:38:12 +0900

    enumeration

---
+++ src/lang/enumeration/ex3/EnumRefMain.java
@@ -0,0 +1,21 @@
+package lang.enumeration.ex3;
+
+public class EnumRefMain {
+
+    public static void main(String[] args) {
+        // class lang.enumeration.ex3.Grade
+        System.out.println("class BASIC = " + Grade.BASIC.getClass());
+        // class lang.enumeration.ex3.Grade
+        System.out.println("class GOLD = " + Grade.GOLD.getClass());
+        // class lang.enumeration.ex3.Grade
+        System.out.println("class DIAMOND = " + Grade.DIAMOND.getClass());
+
+        System.out.println("ref BASIC = " + refValue(Grade.BASIC)); // 30f39991
+        System.out.println("ref GOLD = " + refValue(Grade.GOLD)); // 452b3a41
+        System.out.println("ref DIAMOND = " + refValue(Grade.DIAMOND)); // 4a574795
+    }
+
+    private static String refValue(Object grade) {
+        return Integer.toHexString(System.identityHashCode(grade));
+    }
+}
